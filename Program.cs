9b5e9c2e65316e4c27354b47911034a0926c6d80
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-04 14:53:13 +0530

    Random number on dice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+Random random = new Random();
+int Dice = random.Next(1,7);
+Console.WriteLine("Number on Dice is : " + Dice);
